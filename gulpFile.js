fb693fbd2eeaf8cae041d066ea8148c5a007b05d
Onyinyechukwu Ewelukwa <onyinyeeweluks@gmail.com>
2024-10-04 12:27:32 +0200

    gulp-purgecss plugin and badge component added

--- gulpFile.js
+++ gulpFile.js
@@ -1,14 +1,18 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 // function to compile sass style to css
 function stylesSetUp() {
-  return src("naAfro/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("naAfro/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
 }
 
 // function to watch sass file for changes and recompile it
 function watchStyles() {
-  watch(["naAfro/**/*.scss"], stylesSetUp);
+  watch(["naAfro/**/*.scss", "*.html"], stylesSetUp);
 }
 
 // run the file
